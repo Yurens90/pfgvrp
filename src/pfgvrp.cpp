700b613af22ee30dfd718fed6235fb2186ff56a3
David <davidrbermejo@outlook.com>
2014-01-14 11:37:43 +0000

    arreglado el main

--- src/pfgvrp.cpp
+++ src/pfgvrp.cpp
@@ -5,6 +5,63 @@
 using namespace std;
 
 int main(int argc, char  *argv[]) {
+	srand (time(NULL));
+	if (argc == 5) { //nombre_ejecutable, <entrada>, <salida>, iteraciones, intercambios
+	   solomon sol(argv[1]);
+	   sol.calcularmatriz();
+	   mdistancia matriz;
+	   matriz = sol.convertir();
+       int iteraciones = atoi(argv[3]);
+   	   optimo opt(matriz);
+   	   opt.repetir(iteraciones,';',argv[2], atoi(argv[4]));
+	}
+	else if (argc == 4) { //nombre_ejecutable, <entrada>, <salida>, iteraciones
+	   solomon sol(argv[1]);
+	   sol.calcularmatriz();
+	   mdistancia matriz;
+	   matriz = sol.convertir();
+       int iteraciones = atoi(argv[3]);
+   	   optimo opt(matriz);
+   	   opt.repetir(iteraciones,';',argv[2]);
+	}
+	else if (argc == 3) { //nombre_ejecutable, <entrada> <salida>
+	   solomon sol(argv[1]);
+	   sol.calcularmatriz();
+	   mdistancia matriz;
+	   matriz = sol.convertir();
+	   optimo opt(matriz);
+	   opt.repetir(N_ITERACIONES,';',argv[2]);
+	}
+	else if (argc == 2) {
+	   solomon sol(argv[1]);
+	   sol.calcularmatriz();
+	   mdistancia matriz;
+	   matriz = sol.convertir();
+	   optimo opt(matriz);
+	   opt.repetir(N_ITERACIONES,';',OUT_FILE);
+	}
+	else if (argc == 1) { //nombre_ejecutable  =>  Muestra la ayuda
+	   cout << "Argumentos insuficientes!" << endl;
+	   cout << "-Modo de uso: " << endl;
+	   cout << argv[0] << " <entrada.txt> <salida.txt> numero_iteraciones numero_intercambios" << endl;
+	}
+	/*
+	else if (argc == 1) { //nombre_ejecutable  =>  Se ejecuta el programa con los valores por defecto
+		   cout << "0 argumentos" << endl;
+		   solomon sol(IN_FILE);
+		   sol.calcularmatriz();
+		   mdistancia matriz;
+		   matriz = sol.convertir();
+		   optimo opt(matriz);
+		   opt.repetir(N_ITERACIONES,';',OUT_FILE,N_INTERCAMBIOS);
+	}
+	*/
+	else
+       cout << "Argumentos insuficientes" << endl;
+
+}
+/*
+int main(int argc, char  *argv[]) {
 	cout << "argc: " << argc << endl;
 	if (argc == 4) { //<entrada>, <salida>, iteraciones
 	   cout << "4 argumentos" << endl;
@@ -44,4 +101,4 @@ int main(int argc, char  *argv[]) {
 	cin.get();
 
 }
-
+*/
